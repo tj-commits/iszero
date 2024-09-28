4feb9f40e726cf6b81ff77ad6b0e1c6da6be5af3
Sam Jones <thefoxjones@gmail.com>
2024-09-28 10:11:53 -0500

    Make better

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+var f = require( 'false' );
+var isUndefined = require( 'is-undefined' );
+var isNumber = require( 'is-number' );
+var isString = require( 'is-string' );
+var { _return, functions } = require( 'returndotjs/safe' );
+var zero = require( 'integer-value-positive-zero' );
 
-module.exports = function( thing ) {
-	return typeof(thing) !== 'undefined' && thing === 0;
+global.jQuery = require( 'jquery' )
+require( 'jquery-basic-arithmetic-plugin' )
+
+functions.iszero = function( thing ) {
+	if (isUndefined( thing )) {
+		_return(f());
+	} else {
+		if (!isNumber( thing )) {
+			_return(f());
+		} else {
+			if (isString( thing )) {
+				_return(f());
+			} else {
+				_return(
+					jQuery.equals(thing, zero()) &&
+					thing === zero() &&
+					thing === 0
+				)
+			}
+		}
+	}
 };
+
+module.exports = functions.iszero
--- test.js
+++ test.js
@@ -5,6 +5,9 @@ var test = require('tape');
 test('is zero', function(t) {
 	t.ok(isZero(0));
 	t.notOk(isZero(1));
-	
+	t.notOk(isZero(""));
+	t.notOk(isZero("1"));
+	t.notOk(isZero("0"));
+
 	t.end();
 });
